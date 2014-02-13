72b81846b13ea718d8f4175347e9f80deb85c5f6
SeongHyeok Im <ishez9210@gmail.com>
2014-02-13 04:06:47 -0500

    Commit files updated in the class.

--- inclass6/debugging_exercise_2.py
+++ inclass6/debugging_exercise_2.py
@@ -5,12 +5,16 @@ Created on Sun Feb  9 17:52:54 2014
 @author: pruvolo
 """
 
+import pdb
+
 def factorial(n):
     """ Computes the factorial of the non-negative input integer n """
     return_val = 1
     for i in range(n):
-        return_val *= i
+        print i
+        return_val *= i + 1
     return return_val
 
 if __name__ == '__main__':
-    print factorial(5)
+    v = factorial(5)
+    print "factorial(5):", v
--- inclass6/debugging_exercise_3.py
+++ inclass6/debugging_exercise_3.py
@@ -5,13 +5,17 @@ Created on Sun Feb  9 17:52:54 2014
 @author: pruvolo
 """
 
+import pdb
+
 def get_primes(n):
     """ Returns a list of all prime integers in the range [1,n] """
     return_val = []
-    isPrime = True
+    
+    #pdb.set_trace()
 
-    for i in range(1,n+1):
-        for j in range(1,i):
+    for i in range(2, n + 1):
+        isPrime = True
+        for j in range(2, i):
             if i % j == 0:
                 isPrime = False
         if isPrime:
@@ -21,3 +25,9 @@ def get_primes(n):
 
 if __name__ == '__main__':
     print get_primes(7)
+    print get_primes(1)
+    print get_primes(3)
+    print get_primes(5)
+    print get_primes(33)
+    print get_primes(100)
+    print get_primes(1000)
